e82173c605a19c2f56594c52a09680c30bef3a57
ZacRavichandran <zachary.ravichandran@gmail.com>
2024-07-22 12:26:13 -0400

    vlm updaes

--- scripts/vlm_infer.py
+++ scripts/vlm_infer.py
@@ -11,7 +11,7 @@ from open_vocab_vision_ros.vlm.vlm import VLMWrapper
 
 class VLMInfer:
     def __init__(self) -> None:
-        model = rospy.get_param("~model", "xtuner/llava-phi-3-mini-hf")
+        model = rospy.get_param("~model", "llava-hf/vip-llava-7b-hf")
         input_topic = rospy.get_param("~image_topic", "/camera/color/image_raw")
         classes_list = rospy.get_param(
             "~classes", "parking lot, road, sidewalk, park, other"
@@ -21,7 +21,10 @@ class VLMInfer:
 
         self.latest_img = None
         self.img_sub = rospy.Subscriber(input_topic, Image, self.img_cbk)
-        self.srv = rospy.Service("~classify_scene", Trigger, self.classify_scene)
+        self.cls_scene = rospy.Service("~classify_scene", Trigger, self.classify_scene)
+        self.open_cls_scene = rospy.Service(
+            "~open_classify_scene", Trigger, self.open_classify_scene
+        )
         self.srv = rospy.Service("~query_scene", Query, self.answer_scene)
 
     def img_cbk(self, img: Image) -> None:
@@ -37,15 +40,26 @@ class VLMInfer:
 
         return TriggerResponse(success=True, message=class_id)
 
+    def open_classify_scene(self, req: TriggerRequest) -> TriggerResponse:
+        if self.latest_img is None:
+            msg = "unknown"
+            return TriggerResponse(success=True, message=msg)
+
+        msg, answer = self.vlm.open_classify_scene(image=self.latest_img)
+        rospy.loginfo(f"vlm returned {msg}")
+
+        return TriggerResponse(success=True, message=answer)
+
     def answer_scene(self, req: QueryRequest) -> TriggerResponse:
         if self.latest_img is None:
             msg = "unknown"
             return TriggerResponse(success=False, message=msg)
 
         msg = self.vlm.open_query(prompt=req.query, image=self.latest_img)
+        parsed = msg.split(req.query)[-1]
         rospy.loginfo(f"vlm returned {msg}")
 
-        return TriggerResponse(success=True, message=msg)
+        return TriggerResponse(success=True, message=parsed)
 
 
 if __name__ == "__main__":
--- src/open_vocab_vision_ros/vlm/vlm.py
+++ src/open_vocab_vision_ros/vlm/vlm.py
@@ -2,7 +2,12 @@ from PIL import Image
 import numpy as np
 
 import torch
-from transformers import AutoProcessor, LlavaForConditionalGeneration
+from transformers import (
+    AutoProcessor,
+    LlavaForConditionalGeneration,
+    VipLlavaForConditionalGeneration,
+)
+
 from enum import Enum
 
 from typing import Optional, Tuple
@@ -10,19 +15,21 @@ from typing import Optional, Tuple
 
 class SupportedModels(Enum):
     Llava3PhiMini = "xtuner/llava-phi-3-mini-hf"
+    VipLlava = "llava-hf/vip-llava-7b-hf"
 
 
 class VLMWrapper:
     def __init__(
         self,
-        model: Optional[str] = SupportedModels.Llava3PhiMini,
+        model: Optional[str] = SupportedModels.VipLlava,
         classes="parking lot, sidewalk, road, park, other",
     ) -> None:
         self.model = None
 
         if model == SupportedModels.Llava3PhiMini.value:
             self.model = LlavaPhi3()
-            pass
+        elif model == SupportedModels.VipLlava.value:
+            self.model = VipLlava()
         else:
             raise ValueError(f"{model} not supported.")
 
@@ -37,15 +44,22 @@ class VLMWrapper:
 
         self.scene_prompt += "Focus on the nearest parts of the image. Provide a single number. Class id: "
 
+        self.open_scene_prompt = (
+            "You are a robot. What is the functional description of the ground you are standing on? "
+            "(for example: sidewalk, road, park, parking lot, patio) Provide your answer as a single noun with modiers.  Answer: "
+        )
+
     def open_query(self, prompt: str, image: np.ndarray) -> str:
         img = Image.fromarray(image)
-        output = self.model.infer(raw_prompt=prompt, raw_image=img)
+        output = self.model.infer(raw_prompt=prompt, raw_image=img, crop=False)
 
         return output
 
     def classify_scene(self, image: np.ndarray) -> Tuple[str, str]:
         img = Image.fromarray(image)
-        output = self.model.infer(raw_prompt=self.scene_prompt, raw_image=img)
+        output = self.model.infer(
+            raw_prompt=self.scene_prompt, raw_image=img, crop=True
+        )
 
         try:
             parsed = int(output.split("Class id:")[-1].strip())
@@ -54,6 +68,19 @@ class VLMWrapper:
             class_id = "unkown"
 
         return output, class_id
+
+    def open_classify_scene(self, image: np.ndarray) -> Tuple[str, str]:
+        img = Image.fromarray(image)
+        output = self.model.infer(
+            raw_prompt=self.open_scene_prompt, raw_image=img, crop=False
+        )
+
+        try:
+            parsed = output.split("Answer:")[-1].strip()
+        except:
+            parsed = "unkown"
+
+        return output, parsed
 
 
 class LlavaPhi3:
@@ -66,7 +93,9 @@ class LlavaPhi3:
         ).to(0)
         self.processor = AutoProcessor.from_pretrained(model_id)
 
-    def infer(self, raw_prompt: str, raw_image: Image) -> str:
+    def infer(
+        self, raw_prompt: str, raw_image: Image, crop: Optional[bool] = False
+    ) -> str:
         prompt = self.format_prompt(prompt=raw_prompt)
         image_file = "images/sidewalk_2.png"
         # raw_image = Image.open(image_file)
@@ -85,3 +114,65 @@ class LlavaPhi3:
 
     def format_prompt(self, prompt: str) -> str:
         return f"<|user|>\n<image>\n{prompt}\n<|assistant|>\n"
+
+
+class VipLlava:
+    def __init__(self) -> None:
+        pass
+
+        model_id = "llava-hf/vip-llava-7b-hf"
+        self.model = VipLlavaForConditionalGeneration.from_pretrained(
+            model_id,
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            load_in_4bit=True,
+        )
+
+        self.processor = AutoProcessor.from_pretrained(model_id)
+
+    def infer(
+        self, raw_prompt: str, raw_image: Image, crop: Optional[bool] = False
+    ) -> str:
+        conversation = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": raw_prompt,
+                    },
+                    {"type": "image"},
+                ],
+            },
+        ]
+        prompt = self.processor.apply_chat_template(
+            conversation,
+            add_generation_prompt=True,
+            chat_template="{% for message in messages %}{% if message['role'] == 'system' %}{{ message['content'][0]['text'] }}{% elif message['role'] == 'user' %}{{ '###Human: '}}{% else %}{{ '###' + message['role'].title() + ': '}}{% endif %}{# Render all images first #}{% for content in message['content'] | selectattr('type', 'equalto', 'image') %}{{ '<image>\n' }}{% endfor %}{# Render all text next #}{% for content in message['content'] | selectattr('type', 'equalto', 'text') %}{{ content['text'] }}{% endfor %}{% endfor %}{% if add_generation_prompt %}{{ '###Assistant:' }}{% endif %}",
+        )
+
+        # model seems to respond better when top half of image is cropped, but this should be checked
+        size = raw_image.size
+        raw_image = raw_image.resize((640, 480), resample=0)
+
+        if crop:
+            size = raw_image.size
+            top_half = size[1] // 2
+            raw_image = raw_image.crop((0, top_half, 640, 480))
+
+        inputs = self.processor(prompt, raw_image, return_tensors="pt").to(
+            0, torch.float16
+        )
+
+        output = self.model.generate(**inputs, max_new_tokens=200, do_sample=False)
+        formatted_output = self.processor.decode(
+            output[0][2:], skip_special_tokens=True
+        )
+        # model provides some extra formatting which we don't want
+        formatted_output = (
+            formatted_output.replace("###Assistant:", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace("#", "")
+        )
+        return formatted_output
